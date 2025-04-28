167f16c61e074cf70488a5485a2468b7e594a183
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2025-04-28 14:58:53 +0200

    Refactor InputController: enhance input processing with Lerp for smoother movement

--- src/components/InputController.jsx
+++ src/components/InputController.jsx
@@ -32,18 +32,27 @@ export const InputController = ({ onInputUpdated }) => {
 
         const processInput= ()=>{
             let horizontal = 0;
-            if (keys['ArrowLeft'] || keys['a']) horizontal -= 1;
-            if (keys['ArrowRight'] || keys['d']) horizontal += 1;
+            if (keys['ArrowLeft'] || keys['a']) horizontal -= Scalar.Lerp(0, 1, 0.8);
+            if (keys['ArrowRight'] || keys['d']) horizontal += Scalar.Lerp(0, 1, 0.8);
 
             let vertical = 0;
-            if (keys['ArrowUp'] || keys['w']) vertical += 1;
-            if (keys['ArrowDown'] || keys['s']) vertical -= 1;
+            if (keys['ArrowUp'] || keys['w']) vertical += Scalar.Lerp(0, 1, 0.3);
+            if (keys['ArrowDown'] || keys['s']) vertical -= Scalar.Lerp(0, 1, 0.3);
+
+            let jump = false;
+            if (keys[' ']) jump = true;
 
             const newInputValue = {
                 horizontal,
                 vertical,
+                jump,
             };
 
+            console.group("InputController: processInput");
+            console.log("horizontal", horizontal);
+            console.log("vertical", vertical);
+            console.log("jump", jump);
+            console.groupEnd("InputController: processInput");
             setInputValue(newInputValue);
 
             if (onInputUpdated) {
