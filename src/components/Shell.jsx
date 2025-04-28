f65b51ed5dbe5fa166fd5940a90be54422b91256
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2025-04-28 14:59:02 +0200

    Refactor Shell component: enable player creation callback and restore CameraController import

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { GameObject } from '../contexts/GameObjectContext';
-//import {CameraController} from './CameraController';
+import {CameraController} from './CameraController';
 import {EnvironmentController} from './EnvironnementController';
 import {PlayerController} from './PlayerController';
 import {InputController} from './InputController';
 
 export const Shell = ({ scene, engine }) => {
-
+  const [playerMesh, setPlayerMesh] = useState(null);
   const [input, setInput] = useState({
     horizontal: 0,
     vertical: 0,
@@ -16,11 +16,15 @@ export const Shell = ({ scene, engine }) => {
     setInput(values)
   }
 
+  const onPlayerCreated = (playerMesh) => {
+    setPlayerMesh(playerMesh);
+  };
+
   return (
     <GameObject scene={scene} engine={engine}>
       <EnvironmentController />
-      <PlayerController input={input}/>
-       {/* {playerMesh && <CameraController playerMesh={playerMesh} />} */}
+      <PlayerController input={input} onPlayerCreated={onPlayerCreated}/>
+      {playerMesh && <CameraController playerMesh={playerMesh} /> }
       <InputController onInputUpdated={onInputUpdated } />
     </GameObject>
   );
