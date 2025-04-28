2b0d71a27c2b6037453854bdc7f32ad473addec0
Emanuele Mastaglia <emanuelemastaglia@gmail.com>
2025-04-28 12:30:16 +0200

    Refactor Shell component: update input handling and clean up commented code

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
 import { GameObject } from '../contexts/GameObjectContext';
-import {CameraController} from './CameraController';
+//import {CameraController} from './CameraController';
 import {EnvironmentController} from './EnvironnementController';
 import {PlayerController} from './PlayerController';
 import {InputController} from './InputController';
 
 export const Shell = ({ scene, engine }) => {
 
+  const [input, setInput] = useState({
+    horizontal: 0,
+    vertical: 0,
+  });
+
+  const onInputUpdated = (values) => {
+    setInput(values)
+  }
+
   return (
     <GameObject scene={scene} engine={engine}>
       <EnvironmentController />
-      <PlayerController input={inputValues} onPlayerCreated={handlePlayerCreated} />
-      {playerMesh && <CameraController playerMesh={playerMesh} />}
-      <InputController onUpdate={handleInputUpdated} />
+      <PlayerController input={input}/>
+       {/* {playerMesh && <CameraController playerMesh={playerMesh} />} */}
+      <InputController onInputUpdated={onInputUpdated } />
     </GameObject>
   );
 };
